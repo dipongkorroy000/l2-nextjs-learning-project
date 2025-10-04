6ce32b034921bc5ccfdad0d37248a80965141043
dipongkorroy000 <dipongkorroy000@gmail.com>
2025-10-04 16:47:33 +0600

    add file

--- src/app/(commomLayout)/page.tsx
+++ src/app/(commomLayout)/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 const Homepage = () => {
   return (
     <div>
       <h1 className='text-4xl content-center text-center'>Home page</h1>
+
+       <Button>Click me</Button>
     </div>
   );
 };
